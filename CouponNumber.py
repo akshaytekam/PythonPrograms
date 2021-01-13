ae89c4cfc84ee51bf445300e9551cfa41f178170
Akshay Tekam <akshaytekam@atlassian.com>
2021-01-13 15:01:00 +0530

    Added: file coupon number added in devlopment branch

--- CouponNumber.py
+++ CouponNumber.py
@@ -6,23 +6,19 @@
    * Title - Given N distinct Coupon Numbers, how many random numbers
     do you need to generate distinct coupon number
 """
+from itertools import permutations
 import random
-class CouponNumber:
-    def __init__(self, numberOfCoupon=0, count=0):
-        self.numberOfCoupon=numberOfCoupon
-        self.count=count
-    def getCoupon(self):
-        self.numberOfCoupon=int(input("Enter number of coupon:"))
-        return self.numberOfCoupon
-    def getRandomNum(self):
-        for x in range(self.numberOfCoupon):
-            randomNumber=random.randint(1, 100)
-            count +=1
-            size=randomNumber.__sizeof__()
-        return count
-obj=CouponNumber()
-obj.getCoupon()
-print(obj.getRandomNum())
+import string
+# generating random coupon of given staring
+# and find all possible permutations
+def randomGeneration(size, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))         # generate random value
+ini_str = "abcdefghijklmnopqrst"
+
+N=int(input("Enter length of coupon: "))
+permutation = [''.join(p) for p in permutations(randomGeneration(N, ini_str))]      # generate permutations
+print("Resultant List", str(permutation))
+print("to generate distinct coupon of length "+ str(N)+ " we need "+ str(len(permutation)) + " more coupons")
 
 
 
